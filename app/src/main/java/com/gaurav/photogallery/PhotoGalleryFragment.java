a384866bc6365a4e7f2080c60303e0e01d6944da
Gaurav Singh <gauravs21k@gmail.com>
2022-02-26 00:39:32 +0530

    PhotoGallery app is created

---
+++ app/src/main/java/com/gaurav/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,9 @@
+package com.gaurav.photogallery;
+
+import androidx.fragment.app.Fragment;
+
+public class PhotoGalleryFragment {
+    public static Fragment newInstance() {
+        return new Fragment();
+    }
+}
