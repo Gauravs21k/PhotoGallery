c30bb8e1074fc9a7cfc2c302731b9be9e1edcb84
Gaurav Singh <gauravs21k@gmail.com>
2022-02-26 13:59:00 +0530

    fragment class and view for it added

--- app/src/main/java/com/gaurav/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/gaurav/photogallery/PhotoGalleryFragment.java
@@ -1,9 +1,35 @@
 package com.gaurav.photogallery;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class PhotoGalleryFragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PhotoGalleryFragment extends Fragment{
+    private RecyclerView mPhotoRecyclerView;
+
     public static Fragment newInstance() {
-        return new Fragment();
+        return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mPhotoRecyclerView = (RecyclerView) v.findViewById(R.id.photo_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return v;
     }
 }
