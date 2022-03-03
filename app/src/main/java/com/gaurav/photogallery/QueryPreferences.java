1731d4ef2aac06c0d130702e00eb2bf79e09954f
Gaurav Singh <gauravs21k@gmail.com>
2022-03-04 03:11:08 +0530

    PollService now fetch data from flickr

--- app/src/main/java/com/gaurav/photogallery/QueryPreferences.java
+++ app/src/main/java/com/gaurav/photogallery/QueryPreferences.java
@@ -5,6 +5,7 @@ import android.preference.PreferenceManager;
 
 public class QueryPreferences {
     private  static final String SEARCH_QUERY = "searchQuery";
+    private static final String LAST_RESULT_ID = "lastResultId";
 
     public static String getStoredQuery(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context)
@@ -15,6 +16,18 @@ public class QueryPreferences {
         PreferenceManager.getDefaultSharedPreferences(context)
                 .edit()
                 .putString(SEARCH_QUERY, query)
+                .apply();
+    }
+
+    public static String getLastResultId(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(LAST_RESULT_ID, null);
+    }
+
+    public static void setLastResultID(Context context, String lastResultId) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(LAST_RESULT_ID, lastResultId)
                 .apply();
     }
 }
--- app/src/main/java/com/gaurav/photogallery/services/PollService.java
+++ app/src/main/java/com/gaurav/photogallery/services/PollService.java
@@ -3,8 +3,16 @@ package com.gaurav.photogallery.services;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import com.gaurav.photogallery.FlickrFetcher;
+import com.gaurav.photogallery.GalleryItem;
+import com.gaurav.photogallery.QueryPreferences;
+
+import java.util.List;
 
 public class PollService extends IntentService {
    private static final String TAG = "PollService";
@@ -18,6 +26,40 @@ public class PollService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+       if(!isNetworkAvailableAndConnected()) {
+           return;
+       }
 
+       String query = QueryPreferences.getStoredQuery(this);
+       String lastResultId = QueryPreferences.getLastResultId(this);
+        List<GalleryItem> items;
+
+        if (query == null) {
+            items = new FlickrFetcher().fetchRecentPhotos();
+        } else {
+            items = new FlickrFetcher().searchPhotos(query);
+        }
+
+        if (items.size() == 0) {
+            return;
+        }
+
+        String resultId = items.get(0).getId();
+        if (resultId.equals(lastResultId)) {
+            Log.i(TAG, "Got an old result: " + resultId);
+        } else {
+            Log.i(TAG, "Got a new result: " + resultId);
+        }
+
+        QueryPreferences.setLastResultID(this, resultId);
+    }
+
+    private boolean isNetworkAvailableAndConnected() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        boolean networkAvailableAndConnected = (connectivityManager.getActiveNetwork() != null) &&
+                connectivityManager.getActiveNetworkInfo().isConnected();
+
+        return networkAvailableAndConnected;
     }
 }
