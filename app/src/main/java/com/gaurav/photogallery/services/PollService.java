2d6b75b283a66baf61f36a5f9a56e4239c56fe46
Gaurav Singh <gauravs21k@gmail.com>
2022-03-07 23:31:18 +0530

    notification implemented: PollService sends notification when new results are avilable

--- app/src/main/java/com/gaurav/photogallery/services/PollService.java
+++ app/src/main/java/com/gaurav/photogallery/services/PollService.java
@@ -2,18 +2,26 @@ package com.gaurav.photogallery.services;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.gaurav.photogallery.FlickrFetcher;
 import com.gaurav.photogallery.GalleryItem;
+import com.gaurav.photogallery.PhotoGalleryActivity;
 import com.gaurav.photogallery.QueryPreferences;
+import com.gaurav.photogallery.R;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -49,11 +57,34 @@ public class PollService extends IntentService {
             return;
         }
 
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My notification",
+                    "MyChannel", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
         String resultId = items.get(0).getId();
         if (resultId.equals(lastResultId)) {
             Log.i(TAG, "Got an old result: " + resultId);
         } else {
             Log.i(TAG, "Got a new result: " + resultId);
+
+            Resources resources = getResources();
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pendingIntent = PendingIntent.getService(this, 0, i, 0);
+
+            Notification notification = new NotificationCompat.Builder(this, "My notification")
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))
+                    .setContentText(resources.getString(R.string.new_pictures_text))
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true)
+                    .build();
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(0, notification);
         }
 
         QueryPreferences.setLastResultID(this, resultId);
